7361068db0dfc8bfb5a5fac0a1e9238d3db3b4a0
ivanovaalena <angel40995@inbox.ru>
2023-03-30 21:30:26 +0300

    test1

--- tests/test_demoshop.py
+++ tests/test_demoshop.py
@@ -1,8 +1,7 @@
 from tests.conftest import demoshop
 from selene import have
 import allure
-
-
+from utils.base_session import demoshop
 
 
 def test_cart(chrome_browser):
@@ -13,21 +12,21 @@ def test_cart(chrome_browser):
     chrome_browser.open('')
     chrome_browser.element('#topcartlink .cart-qty').click()
     chrome_browser.element('#topcartlink .cart-qty').should(have.text('(1)'))
-    with allure.step('Deleting products from cart'):
-        chrome_browser.open('')
-        chrome_browser.element('.ico-cart .cart-label').click()
-        chrome_browser.element('.qty-input').clear().set_value('0')
-        chrome_browser.element('.update-cart-button').click()
+    chrome_browser.open('')
+    chrome_browser.element('.ico-cart .cart-label').click()
+    chrome_browser.element('.qty-input').clear().set_value('0')
+    chrome_browser.element('.update-cart-button').click()
+
 
 def test_wishlist(chrome_browser):
-    with allure.step('Adding product to Wishlist'):
+
         response = demoshop.post('/addproducttocart/details/14/2')
         assert response.status_code == 200
         chrome_browser.open('')
         chrome_browser.element('.ico-wishlist .wishlist-qty').should(have.text('(1)')).click()
         chrome_browser.element('.cart-item-row .product').should(have.text('Black & White Diamond Heart'))
 
-    with allure.step('Deleting products from wishlist'):
+
         chrome_browser.open('')
         chrome_browser.element('.ico-wishlist .wishlist-qty').click()
         chrome_browser.element('[name="removefromcart"]').click()
--- utils/base_session.py
+++ utils/base_session.py
@@ -1,5 +1,41 @@
 from requests import Session, Response
 import allure
+import json
+import logging
+from curlify import to_curl
+from json import JSONDecodeError
+
+
+def allure_logger(func):
+    def wrapper(*args, **kwargs):
+        method, url = args[1], args[2]
+
+        with allure.step(f"{method} {url}"):
+            response: Response = func(*args, **kwargs)
+
+            allure.attach(body=to_curl(response.request).encode('utf8'), name=f"Request: {response.status_code}",
+                          attachment_type=allure.attachment_type.TEXT, extension='.txt')
+
+            try:
+                allure.attach(body=json.dumps(response.json(), indent=4), name=f"Response: {response.status_code}",
+                              attachment_type=allure.attachment_type.JSON, extension='.json')
+            except JSONDecodeError:
+                allure.attach(body=response.text, name=f"Response: {response.status_code}",
+                              attachment_type=allure.attachment_type.TEXT, extension='.txt')
+
+            return response
+
+    return wrapper
+
+
+def request_logging(func):
+    def wrapper(*args, **kwargs):
+        response: Response = func(*args, **kwargs)
+        logging.info(f"status code is {response.status_code} - {to_curl(response.request)}")
+
+        return response
+
+    return wrapper
 
 
 class BaseSession(Session):
@@ -7,6 +43,12 @@ class BaseSession(Session):
         super(BaseSession, self).__init__()
         self.url = url
 
+    @allure_logger
+    @request_logging
     def request(self, method, url, **kwargs) -> Response:
-        with allure.step(f"{method} {url}"):
-            return super().request(method, self.url + url, **kwargs)
+        response = super().request(method, self.url + url, **kwargs)
+        return response
+
+
+regres = BaseSession('https://reqres.in/api')
+demoshop = BaseSession('https://demowebshop.tricentis.com')
